e8c0c210c63bfa53f747b97d316794ec7789cc85
Narek Yegoryan <yegoryan.narek@gmail.com>
2018-08-19 02:59:42 -0700

    Add project files

---
+++ RemoteRegistryEditor/RemoteRegistryEditor.Client/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RemoteRegistryEditor.Client
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        void Notify([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
