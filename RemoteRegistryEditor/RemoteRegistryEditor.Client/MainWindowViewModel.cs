afc8a84076ef5f7ee6b204408434fc8a7da47882
Narek Yegoryan <yegoryan.narek@gmail.com>
2018-08-19 07:05:33 -0700

    Changes

--- RemoteRegistryEditor/RemoteRegistryEditor.Client/MainWindowViewModel.cs
+++ RemoteRegistryEditor/RemoteRegistryEditor.Client/MainWindowViewModel.cs
@@ -5,14 +5,11 @@ namespace RemoteRegistryEditor.Client
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-
         void Notify([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-
     }
 }
