da0cb668cee1582ba2eeb808208ce6ace27999bc
narekye <narek@TESTCAPP-PC>
2019-01-13 03:23:16 -0500

    Add Windows license checker

---
+++ LicenseChecker/LicenseChecker.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using LicenseChecker.Service;
+using System;
+
+namespace LicenseChecker.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new LicenseCheckerService();
+            var checkedComputers = service.RunWindowsLicenseCheck();
+
+            foreach (var computer in checkedComputers)
+            {
+                Console.WriteLine($"{computer.Key} | {computer.Value}");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ LicenseChecker/LicenseChecker.Service/LicenseCheckerService.cs
@@ -0,0 +1,62 @@
+﻿using ProcessController.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+
+namespace LicenseChecker.Service
+{
+    public class LicenseCheckerService
+    {
+        private Library _library;
+        private IEnumerable<string> _computersToBeChecked;
+
+        public LicenseCheckerService(IEnumerable<string> computers = null)
+        {
+            _library = new Library();
+
+            if (computers == null)
+                computers = _library.GetCurrentDomainComputers();
+
+            _computersToBeChecked = computers;
+            _computersToBeChecked = _computersToBeChecked.Where(x => x.EndsWith("pc", StringComparison.InvariantCultureIgnoreCase)).ToList();
+        }
+
+
+        public Dictionary<string, LicenseStatusTypeEnum> RunWindowsLicenseCheck()
+        {
+            var result = new Dictionary<string, LicenseStatusTypeEnum>();
+
+            foreach (var computer in _computersToBeChecked)
+            {
+                var alive = _library.Ping(computer, 10);
+
+                if (!alive)
+                    continue;
+
+                try
+                {
+                    var scope = new ManagementScope(@"\\" + computer + @"\root\cimv2", null);
+                    var queryString = "SELECT * FROM SoftwareLicensingProduct WHERE ApplicationID = '55c92734-d682-4d71-983e-d6ec3f16059f' and LicenseStatus = 1";
+                    var query = new ObjectQuery(queryString);
+
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+                    var searcherResult = searcher.Get();
+
+                    using (searcherResult)
+                    {
+                        var isActivated = Convert.ToInt32(searcherResult.Count > 0);
+                        result.Add(computer, (LicenseStatusTypeEnum)isActivated);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
